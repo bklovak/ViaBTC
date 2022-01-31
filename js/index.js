a6fd225ea6cc11eaf82f09260a7670f306a70856
bklovak <klovak.bohdan@gmail.com>
2022-01-31 22:27:22 +0200

    fix problemx

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/** @format */
+
+let a = { name: "Bohdan", age: 21, surname: "BohomDan" };
+
+let name = "Bohdan";
+let age = 21;
+let surname = "BohomDan";
+
+a.age = "Vasul";
+console.log(a);
+
+const b = ["Bohdan", "Ahuennui"];
+console.log(b[1]);
